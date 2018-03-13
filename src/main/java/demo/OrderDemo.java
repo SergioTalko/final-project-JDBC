1301fbaf742a3c633fb4b5881e39f8a6bc8f057f
Sergio Talko <sergio.talko@gmail.com>
2018-03-13 10:56:12 +0200

    init

--- src/main/java/demo/OrderDemo.java
+++ src/main/java/demo/OrderDemo.java
@@ -10,7 +10,7 @@ public class OrderDemo {
     public static void main(String[] args) throws Exception {
 
         OrderService orderService = new OrderService();
-        orderService.bookRoom(2555,648,new Date());
+//        orderService.bookRoom(2555,648,new Date());
 //        orderService.cancelReservation(2555,648);
     }
 }
