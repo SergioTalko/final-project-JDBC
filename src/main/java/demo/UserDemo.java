7b2615de4d9e602f4a613ca8af77962130fee7a4
PC <s>
2018-03-13 10:20:34 +0200

    init commit

--- src/main/java/demo/UserDemo.java
+++ src/main/java/demo/UserDemo.java
@@ -4,11 +4,12 @@ package demo;
 import controller.UserController;
 import dao.UserDAO;
 import entity.User;
+import entity.UserType;
 
 public class UserDemo {
     public static void main(String[] args) throws Exception {
 
-        User user = new User("Test","hello","UA");
+        User user = new User("Test2","hello","UA", UserType.ADMIN);
         UserDAO userDAO = new UserDAO();
         userDAO.add(user);
 
